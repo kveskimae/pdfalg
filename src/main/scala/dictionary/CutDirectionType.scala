860aeea419a8be701bd00581e27af379e1dd7ba0
Kristjan Veskimae <kristjan.veskimae@gmail.com>
2017-05-18 22:17:12 +0100

    Add positional comparing (without real locations)

---
+++ src/main/scala/dictionary/CutDirectionType.scala
@@ -0,0 +1,33 @@
+package dictionary
+
+sealed trait CutDirectionType
+
+/**
+  *
+  * |-----|
+  * |  x  |
+  * |  x  |
+  * |  x  |
+  * |  x  |
+  * |  x  |
+  * |_____|
+  *
+  * Cutting along the vertical line
+  *
+  */
+case object VERTICAL extends CutDirectionType
+
+/**
+  *
+  * |-----|
+  * |     |
+  * |     |
+  * |xxxxx|
+  * |     |
+  * |     |
+  * |_____|
+  *
+  * Cutting along the horizontal line
+  *
+  */
+case object HORIZONTAL extends CutDirectionType
