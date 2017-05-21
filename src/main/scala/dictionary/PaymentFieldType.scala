b417826b23fda572f84b040b736de17c606aa25e
Kristjan Veskimae <kristjan.veskimae@gmail.com>
2017-05-21 23:05:38 +0100

    Add conversion from String to PaymentFieldType

--- src/main/scala/dictionary/PaymentFieldType.scala
+++ src/main/scala/dictionary/PaymentFieldType.scala
@@ -11,3 +11,20 @@ case object TOTAL_BEFORE_TAXES extends PaymentFieldType
 case object ISSUE_DATE extends PaymentFieldType
 case object DUE_DATE extends PaymentFieldType
 case object VATIN extends PaymentFieldType
+
+object PaymentFieldType {
+
+  implicit def convert(name: String): PaymentFieldType = name match {
+    case "IBAN" => IBAN
+    case "INVOICE_ID" => INVOICE_ID
+    case "NAME" => NAME
+    case "REFERENCE_NUMBER" => REFERENCE_NUMBER
+    case "TOTAL" => TOTAL
+    case "TOTAL_BEFORE_TAXES" => TOTAL_BEFORE_TAXES
+    case "ISSUE_DATE" => ISSUE_DATE
+    case "DUE_DATE" => DUE_DATE
+    case "VATIN" => VATIN
+    case _ => throw new IllegalArgumentException("Unknown payment field type name: " + name)
+  }
+
+}
