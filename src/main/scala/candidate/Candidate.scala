b0b7a71a4e1802ccf4293774b462c368b43fcea1
Kristjan Veskimae <kristjan.veskimae@gmail.com>
2017-06-23 23:43:21 +0100

    Make Candidate's value accessible through JavaBean getter

--- src/main/scala/candidate/Candidate.scala
+++ src/main/scala/candidate/Candidate.scala
@@ -6,12 +6,14 @@ import dictionary.PropertyType
 import org.apache.commons.lang3.builder.{HashCodeBuilder, ReflectionToStringBuilder, ToStringStyle}
 import org.pdfextractor.db.domain.dictionary.PaymentFieldType
 
+import scala.beans.BeanProperty
+
 /*
 * Note: Natural orderings in subclass implementations of Comparable are not consistent with equals.
 *
 * Also rearranges more likely candidates to the start of the collection.
 */
-class Candidate(val value: Any,
+class Candidate(@BeanProperty val value: Any,
                 val x: Integer,
                 val y: Integer,
                 val bold: Boolean,
