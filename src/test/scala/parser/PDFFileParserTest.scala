942283563318357c2ca0458adeaecc2b98d9dce7
Kristjan Veskimae <kristjan.veskimae@gmail.com>
2017-06-27 00:15:52 +0100

    Add more test cases for pdf file parser

--- src/test/scala/parser/PDFFileParserTest.scala
+++ src/test/scala/parser/PDFFileParserTest.scala
@@ -2,6 +2,7 @@ package parser
 
 import java.io.InputStream
 
+import exception.AppBadInputException
 import org.junit.runner.RunWith
 import org.scalatest._
 import org.scalatest.junit.JUnitRunner
@@ -20,6 +21,22 @@ class PDFFileParserTest  extends FlatSpec with Matchers {
     assert(85 == firstPhrase.y)
     assert(1 == firstPhrase.pageNumber)
     assert("Adobe Acrobat PDF Files" == firstPhrase.text)
+  }
+
+  "A PDFFileParser" should "extract text" in {
+    val contentStream: InputStream = Thread.currentThread.getContextClassLoader.getResourceAsStream("parser_example_invoices/pdf-sample.pdf")
+    val parsed: ParseResult = PDFFileParser.parse(contentStream)
+    parsed.text should not be (null)
+    assert(parsed.text.startsWith("Adobe Acrobat PDF Files"))
+  }
+
+  "A PDFFileParser" should "throw with corrupt PDF file" in {
+    val contentStream: InputStream = Thread.currentThread.getContextClassLoader.getResourceAsStream("parser_example_invoices/pdf-corrupt.pdf")
+
+    an [AppBadInputException] should be thrownBy {
+      PDFFileParser.parse(contentStream)
+    }
+
   }
 
 }
