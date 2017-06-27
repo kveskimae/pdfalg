0571bfeb32a66a912c63d88da2a673b38dcddd74
Kristjan Veskimae <kristjan.veskimae@gmail.com>
2017-06-27 23:01:51 +0100

    Add phrase package to extractor config

--- src/main/scala/config/ExtractorConfig.scala
+++ src/main/scala/config/ExtractorConfig.scala
@@ -3,7 +3,8 @@ package config
 import finder.FinderPackageMarker
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import phrase.PhrasePackageMarker
 
 @Configuration
-@ComponentScan(basePackageClasses = Array(classOf[FinderPackageMarker]))
+@ComponentScan(basePackageClasses = Array(classOf[FinderPackageMarker], classOf[PhrasePackageMarker]))
 class ExtractorConfig {}
