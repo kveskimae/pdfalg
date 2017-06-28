63fb94c4bb9cfae3ed517df50a0d1f7159cab5da
Kristjan Veskimae <kristjan.veskimae@gmail.com>
2017-06-28 23:39:29 +0100

    Use ScalaTest

--- src/test/scala/finder/AbstractInvoiceFileReader.scala
+++ src/test/scala/finder/AbstractInvoiceFileReader.scala
@@ -1,25 +1,11 @@
 package finder
 
-import java.util.Locale
-
-import config.ExtractorConfig
-import io.IOHelper
-import org.junit.runner.RunWith
-import org.pdfextractor.db.config.{JpaConfig, StandaloneDataConfig}
-import org.pdfextractor.db.domain.dictionary.SupportedLocales
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.test.context.{ActiveProfiles, ContextConfiguration}
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
-
 object AbstractInvoiceFileReader {
 
   val Starman = "et/Starman.pdf"
 
 }
-
-@RunWith(classOf[SpringJUnit4ClassRunner])
-@ContextConfiguration(classes=Array(classOf[JpaConfig], classOf[StandaloneDataConfig], classOf[ExtractorConfig]))
-@ActiveProfiles(Array("unittest"))
+/*
 trait AbstractInvoiceFileReader  {
 
   @Autowired var finderFactory: FinderFactory = _
@@ -46,3 +32,4 @@ trait AbstractInvoiceFileReader  {
   }
 
 }
+*/
--- src/test/scala/finder/et/EstonianAccountNumberFinderTest.scala
+++ src/test/scala/finder/et/EstonianAccountNumberFinderTest.scala
@@ -1,68 +1,56 @@
 package finder.et
 
-import java.io.IOException
-
 import candidate.Candidate
 import config.ExtractorConfig
-import finder.AbstractInvoiceFileReader
+import finder.{AbstractInvoiceFileReader, FinderFactory}
 import io.IOHelper
-import org.junit.Assert._
-import org.junit.Test
-import org.junit.runner.RunWith
 import org.pdfextractor.db.config.{JpaConfig, StandaloneDataConfig}
+import org.scalatest.{FlatSpec, Matchers}
 import org.slf4j.{Logger, LoggerFactory}
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
-import org.springframework.test.context.{ActiveProfiles, ContextConfiguration}
+import org.springframework.test.context.{ActiveProfiles, ContextConfiguration, TestContextManager}
 import parser.{PDFFileParser, ParseResult}
 import phrase.PhraseTypesStore
 
 import scala.collection.LinearSeq
 
-@RunWith(classOf[SpringJUnit4ClassRunner])
 @ContextConfiguration(classes=Array(classOf[JpaConfig], classOf[StandaloneDataConfig], classOf[ExtractorConfig]))
 @ActiveProfiles(Array("unittest"))
-class EstonianAccountNumberFinderTest extends AbstractInvoiceFileReader {
+class EstonianAccountNumberFinderTest extends FlatSpec with Matchers {
 
   val log: Logger = LoggerFactory.getLogger(classOf[PhraseTypesStore])
 
+  @Autowired var finderFactory: FinderFactory = _
+
   @Autowired var estonianAccountNumberFinder: EstonianAccountNumberFinder = _
 
-  @Test
-  @throws[IOException]
-  def testFindsAccountsFromRealPDF(): Unit = {
+  new TestContextManager(this.getClass()).prepareTestInstance(this)
+
+  "An Estonian account number finder" should "find accounts from real PDF" in {
     val inputStream = IOHelper.getInputStreamFromFile(AbstractInvoiceFileReader.Starman)
     val parseResult = PDFFileParser.parse(inputStream)
     val candidates = estonianAccountNumberFinder.findCandidates(parseResult)
-    assertFalse(candidates.isEmpty)
-    assertEquals(4, candidates.size)
+    assert(!candidates.isEmpty)
+    assert(4 == candidates.size)
     val foundValues: Seq[String] = candidates.map(_.getValue.asInstanceOf[String])
-    assertTrue(foundValues.contains("EE882200001180000796"))
-    assertTrue(foundValues.contains("EE921010002046022001"))
-    assertTrue(foundValues.contains("EE103300332097940003"))
-    assertTrue(foundValues.contains("EE561700017000030979"))
-
-    log.info("Tested Estonian accounts from real PDF")
+    assert(foundValues.contains("EE882200001180000796"))
+    assert(foundValues.contains("EE921010002046022001"))
+    assert(foundValues.contains("EE103300332097940003"))
+    assert(foundValues.contains("EE561700017000030979"))
   }
 
-  @Test
-  @throws[IOException]
-  def testFindsAccountsFromString(): Unit = {
+  "An Estonian account number finder" should "search accounts from the invoice string" in {
     val invoiceAsString = IOHelper.getStringFromFile("EestiEnergia.txt")
     val candidates: Seq[Candidate] = estonianAccountNumberFinder.findCandidates(new ParseResult(invoiceAsString, LinearSeq.empty))
     val foundValues: Seq[String] = candidates.map(_.getValue.asInstanceOf[String])
-
-    assertFalse(foundValues.isEmpty)
-
-    assertTrue(foundValues.contains("EE232200001180005555"))
-    assertTrue(foundValues.contains("EE081010002059413005"))
-    assertTrue(foundValues.contains("EE703300332099000006"))
-    assertTrue(foundValues.contains("EE431700017000115797"))
+    assert(!foundValues.isEmpty)
+    assert(foundValues.contains("EE232200001180005555"))
+    assert(foundValues.contains("EE081010002059413005"))
+    assert(foundValues.contains("EE703300332099000006"))
+    assert(foundValues.contains("EE431700017000115797"))
   }
 
-  @Test
-  @throws[IOException]
-  def testDoesNotFindIncorrectAccounts(): Unit = {
+  "An Estonian account number finder" should "not find incorrect accounts" in {
     val invoiceAsString = IOHelper.getStringFromFile("RiggedInvoice.txt")
     val candidates: Seq[Candidate] = estonianAccountNumberFinder.findCandidates(new ParseResult(invoiceAsString, LinearSeq.empty))
     assert(candidates.isEmpty)
